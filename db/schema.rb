b34ac11f74be7c3b41de32206d66ee3deaa695f2
shibin <910872228@qq.com>
2022-10-01 15:33:08 +0800

    implement article crud

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221001023942) do
+ActiveRecord::Schema.define(version: 20221001061917) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
