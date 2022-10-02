fd523ba6da4b13d8c06e459bf90c3e0236534d6c
shibin <910872228@qq.com>
2022-10-02 08:41:37 +0800

    implement part 2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,13 @@ Rails.application.routes.draw do
   devise_for :users
   resources :articles
   root"welcome#index"
+
+namespace :admin do
+  resources :articles
+end
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
---
+++ db/migrate/20221002001536_add_is_admin_to_user.rb
@@ -0,0 +1,8 @@
+class AddIsAdminToUser < ActiveRecord::Migration[5.0]
+  def change
+
+      add_column :users, :is_admin, :boolean, default: false
+
+  
+  end
+end
