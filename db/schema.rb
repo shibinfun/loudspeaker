2751e5330a4b42596367b373a2c4e39c097df24b
shibin <910872228@qq.com>
2022-10-05 06:52:46 +0800

    update ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -41,12 +41,7 @@ ActiveRecord::Schema.define(version: 20221003112926) do
     t.string   "attachment"
   end
 
-  create_table "information", force: :cascade do |t|
-    t.string   "title"
-    t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+
 
   create_table "informationns", force: :cascade do |t|
     t.string   "title"
