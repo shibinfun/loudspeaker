f0489f70269d9bc033a0f0011f63af624913a1d2
yfscret <305667862@qq.com>
2022-10-19 17:00:31 +0800

    fx

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,7 +21,7 @@ set :repo_url, "git@github.com:shibinfun/loudspeaker.git"
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", 'config/master.key', 'db/production.sqlite3'
+append :linked_files, "config/database.yml", 'config/master.key'
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "tmp/webpacker", "public/system", "vendor", "storage"
