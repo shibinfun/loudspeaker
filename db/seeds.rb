c4f205dff51b541ef6f3eb748b51f4d8e0019fea
shibin <910872228@qq.com>
2022-11-24 15:19:44 +0800

    更改密码

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,9 +8,9 @@
 u = User.new
 u.email = "iloveshibin@gmail.com"           # 可以改成自己的 email
 
-u.password = "123456"                # 最少要六码
+u.password = "iloveshibin"                # 最少要六码
 
-u.password_confirmation = "123456"   # 最少要六码
+u.password_confirmation = "iloveshibin"   # 最少要六码
 
 u.is_admin = true
 u.save
