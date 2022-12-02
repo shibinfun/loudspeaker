23328752b89b695eada58d0e9d66fc653a689df3
shibin <910872228@qq.com>
2022-12-02 19:36:02 +0800

    1

--- app/controllers/recommends_controller.rb
+++ app/controllers/recommends_controller.rb
@@ -1,6 +1,12 @@
 class RecommendsController < ApplicationController
   def index
-    @recommends = Recommend.all
+  @a = RecommendCategory.all
+  if params[:recommend].blank?
+  @recommends = Recommend.all
+  else
+  #@ac = recommendCategory.find_by(params[:category])
+  @recommends = Recommend.where(recommend_category_id: params[:recommend])
+  end
   end
 
   def show
