3be228ddc62a32150f3f653d46cf83fa0e86f16c
shibin <910872228@qq.com>
2022-12-12 18:21:13 +0800

    12.12

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-      flash[:notice] = "本网站刚刚上线，内容不足，分享知识，现在多数文章是石斌自己写的，非常枯燥乏味，也几乎没用，其实，最有用的知识在|有用网址|栏目里面"
+      flash[:notice] = "有用网址，里面的东西比较有用"
     @asks = Ask.all
     @articles = Article.all
   end
